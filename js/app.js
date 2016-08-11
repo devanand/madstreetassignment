a470a7dc58795442485ac8b45c6f83f83130edf0
devanand <anand.dev.1985@gmail.com>
2016-08-11 23:47:28 +0530

    Added sorting using price and score

--- js/app.js
+++ js/app.js
@@ -130,8 +130,29 @@ app.controller('shopController', function($scope, $http) {
         }
         
         $scope.filterProducts();
+    },
+    
+    
+    $scope.sortScore = function() {
+        $scope.selectedItems.sort(sort_by("score", true, parseInt));
     }
     
+    $scope.sortPrice = function() {
+        $scope.selectedItems.sort(sort_by("price", false, parseInt));
+    }
+    
+    var sort_by = function(field, reverse, primer){
+
+       var key = primer ? 
+       function(x) {return primer(x[field])} : 
+       function(x) {return x[field]};
+
+       reverse = !reverse ? 1 : -1;
+
+       return function (a, b) {
+          return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+       } 
+   }
 });
 
 app.filter('startFrom', function() {
